6711806949a3a5c3594840da971603fc09b9f712
elianatasiei <elianatasiei@gmail.com>
2023-10-16 17:28:06 +0100

    header and cursor styles

---
+++ script.js
@@ -0,0 +1,9 @@
+const cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  cursor.style.left = `${x}px`;
+  cursor.style.top = `${y}px`;
+});
